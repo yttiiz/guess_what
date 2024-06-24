2f3655bae5bfb1d8617537834cdfd70fdf84ee1f
yttiiz <dominique.talis@proton.me>
2024-06-24 11:55:00 +0200

    refactor(utils): improve utils objects

--- src/main/kotlin/utils/DateHandler.kt
+++ src/main/kotlin/utils/DateHandler.kt
@@ -7,62 +7,60 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.*
 
-class DateHandler {
-    companion object {
-        private fun convertStr(it: String): CharSequence {
-            return if (it.length < 2) "0$it" else it
+object DateHandler {
+    private fun convertStr(it: String): CharSequence {
+        return if (it.length < 2) "0$it" else it
+    }
+
+    private fun formatDateCorrectly(birth: Date): String {
+        return birth
+            .toInstant()
+            .toString()
+            .split("T")[0]
+            .split("-")
+            .joinToString(
+                separator = "-",
+                transform = { convertStr(it) }
+            )
+    }
+
+    private fun formatStringDateCorrectly(birth: String): String {
+        return birth
+            .split("-")
+            .joinToString(
+                separator = "-",
+                transform = { convertStr(it) }
+            )
+    }
+
+    fun getAge(birth: Any): String {
+        return when(birth) {
+            is Date -> "${Period.between(
+                LocalDate.parse(formatDateCorrectly(birth)),
+                LocalDate.now(),
+            ).years} ans"
+
+            is String -> "${Period.between(
+                LocalDate.parse(formatStringDateCorrectly(birth)),
+                LocalDate.now(),
+            ).years} ans"
+
+            is Number -> "$birth ans"
+
+            else -> "not a birth"
         }
+    }
 
-        private fun formatDateCorrectly(birth: Date): String {
-            return birth
-                .toInstant()
-                .toString()
-                .split("T")[0]
-                .split("-")
-                .joinToString(
-                    separator = "-",
-                    transform = { convertStr(it) }
-                )
-        }
-
-        private fun formatStringDateCorrectly(birth: String): String {
-            return birth
-                .split("-")
-                .joinToString(
-                    separator = "-",
-                    transform = { convertStr(it) }
-                )
-        }
-
-        fun getAge(birth: Any): String {
-            return when(birth) {
-                is Date -> "${Period.between(
-                    LocalDate.parse(formatDateCorrectly(birth)),
-                    LocalDate.now(),
-                ).years} ans"
-
-                is String -> "${Period.between(
-                    LocalDate.parse(formatStringDateCorrectly(birth)),
-                    LocalDate.now(),
-                ).years} ans"
-
-                is Number -> "$birth ans"
-
-                else -> "not a birth"
-            }
-        }
-
-        fun getBirth(
-            birth: Date,
-            locale: Locale = Locale.FRANCE
-        ): String {
-            return LocalDate
-                .parse(formatDateCorrectly(birth))
-                .format(
-                    DateTimeFormatter
-                        .ofLocalizedDate(FormatStyle.LONG)
-                        .withLocale(locale)
-                )
-        }
+    fun getBirth(
+        birth: Date,
+        locale: Locale = Locale.FRANCE
+    ): String {
+        return LocalDate
+            .parse(formatDateCorrectly(birth))
+            .format(
+                DateTimeFormatter
+                    .ofLocalizedDate(FormatStyle.LONG)
+                    .withLocale(locale)
+            )
     }
 }
--- src/main/kotlin/utils/ImageHandler.kt
+++ src/main/kotlin/utils/ImageHandler.kt
@@ -3,12 +3,12 @@ package quiz.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import org.jetbrains.skia.Image
-import java.net.URL
+import java.net.URI
 
 object ImageHandler {
 
     @Composable
     fun loadFromUrl(url: String) = Image
-        .makeFromEncoded(URL(url).readBytes())
+        .makeFromEncoded(URI(url).toURL().readBytes())
         .toComposeImageBitmap()
 }
